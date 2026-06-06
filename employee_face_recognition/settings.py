057fa2b9eca553a1ac847eb097f8eeb0b70949a5
ithouseacademy <ithouselevelup@gmail.com>
2026-06-06 12:24:24 +0500

    face id it house academy 1.1 versiya \

--- employee_face_recognition/settings.py
+++ employee_face_recognition/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-your-secret-key-change-this'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "faceid11-production.up.railway.app",
