43cf28a17c7a494bb07363233b72361415cfe65d
ithouseacademy <ithouselevelup@gmail.com>
2026-06-06 16:59:28 +0500

    face id it house academy 1.1 versiya \

--- employee_face_recognition/settings.py
+++ employee_face_recognition/settings.py
@@ -110,11 +110,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
 }
 
